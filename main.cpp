c2f935ad21fbe53f109665570b6141574646a1df
Sylvain <sylvaindu05@yahoo.fr>
2020-03-19 11:59:37 +0100

    Modification relatives à ma branche pour tests Donc peu importante pour l'avancement du projet

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
+#include "signinsignupdialog.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
+
+    SigninSignupDialog d;
+    d.show();
+
+
     return a.exec();
 }
